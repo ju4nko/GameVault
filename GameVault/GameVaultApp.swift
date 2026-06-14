b33d98f4268cae6d0d75b41840b8e3141f6266a0
ju4nko <ju4nko@gmail.com>
2026-06-14 10:24:35 +0200

    Initial Commit

---
+++ GameVault/GameVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameVaultApp.swift
+//  GameVault
+//
+//  Created by Juanjo on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct GameVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
