5bf638f931610289d52abec645e2235db20f8d62
ju4nko <ju4nko@gmail.com>
2026-06-14 17:23:04 +0200

    Juegos harcodeados

--- GameVault/Models/Game.swift
+++ GameVault/Models/Game.swift
@@ -56,3 +56,20 @@ struct Game: Identifiable, Hashable, Codable {
     var notes: String?
     
 }
+
+extension Game {
+    static let sampleGames: [Game] =
+        [Game(rawgID: nil, title: "Hollow Knight", platform: .nintendoSwitch, status: .playing, rating: 5, hoursPlayed: 120.5, coverArtURL: nil, completionDate: nil, notes: "Juego de plataformas de acción en el que controlas a un personaje hueco El Hollow Knight, que puede desciende a través de las sombras para atacar."),
+         
+         Game(rawgID: nil, title: "Clair Obscure: Expedition 33", platform: .pc, status: .completed, rating: 5, hoursPlayed: 60.5, coverArtURL: nil, completionDate: Date(timeIntervalSinceNow: -86400 * 30), notes: "Menudo drama de juego. Además de parrys muy adictivos, hay un sistema de combate muy bien hecho."),
+         
+         Game(rawgID: nil, title: "AstroBot", platform: .ps5, status: .wishlist, rating: nil, hoursPlayed: 0.0, coverArtURL: nil, completionDate: nil, notes: "No lo sé, aún no he jugado. Pero es un juego de acción de combate en primera persona en el que controlas a un robot que se convierte en un humano para proteger a la humanidad."),
+         
+         Game(rawgID: nil, title: "Disco Elysium", platform: .ps4, status: .abandoned, rating: 5, hoursPlayed: 20.0, coverArtURL: nil, completionDate: nil, notes: "Aventura conversacional que trata sobre un señor ahorcado y tu tienes que investigar su asesinato. Muy divertido."),
+         
+         Game(rawgID: nil, title: "Batman Arkham Asylum", platform: .xboxSeries, status: .backlog, rating: 4, hoursPlayed: 20.5, coverArtURL: nil, completionDate: nil, notes: "Contorlas a batman. Nada más que argumentar"),
+         
+         Game(rawgID: nil, title: "Forza Horizon 5", platform: .xboxOne, status: .playing, rating: 3, hoursPlayed: 12.5, coverArtURL: nil, completionDate: nil, notes: "Son coches. Muy buenos. Muy buenos. Muy buenos."),
+         
+         Game(rawgID: nil, title: "Pragmata", platform: .mobile, status: .playing, rating: 1, hoursPlayed: 2, coverArtURL: nil, completionDate: nil, notes: "Como va a estar pragmata en mobile. Te estoy tomando el pelo. Juegazao por cierto.")]
+}
